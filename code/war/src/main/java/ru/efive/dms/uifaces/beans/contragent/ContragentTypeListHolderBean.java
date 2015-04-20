3e08e996d2250947e69775136d5474802c709afa
Upatov Egor <EUpatov@korusconsulting.ru>
2015-04-20 15:17:07 +0300

    contragentType dao + view

--- code/war/src/main/java/ru/efive/dms/uifaces/beans/contragent/ContragentTypeListHolderBean.java
+++ code/war/src/main/java/ru/efive/dms/uifaces/beans/contragent/ContragentTypeListHolderBean.java
@@ -1,19 +1,17 @@
 package ru.efive.dms.uifaces.beans.contragent;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import ru.efive.dms.uifaces.beans.SessionManagementBean;
+import ru.efive.dms.uifaces.beans.abstractBean.AbstractDocumentLazyDataModelBean;
+import ru.efive.dms.uifaces.lazyDataModel.referencebook.LazyDataModelForContragentType;
 import ru.efive.dms.util.ApplicationDAONames;
 import ru.efive.sql.dao.RbContragentTypeDAOImpl;
-import ru.efive.uifaces.bean.AbstractDocumentListHolderBean;
 import ru.entity.model.crm.ContragentType;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Author: Upatov Egor <br>
@@ -23,52 +21,16 @@ import java.util.List;
  */
 @ManagedBean(name = "contragentTypeList")
 @ViewScoped
-public class ContragentTypeListHolderBean extends AbstractDocumentListHolderBean<ContragentType> {
-
-
-    private static final Logger logger = LoggerFactory.getLogger("RB_CONTRAGENT_TYPE");
-
-    private String filter;
-
+public class ContragentTypeListHolderBean extends AbstractDocumentLazyDataModelBean<ContragentType>{
+    private RbContragentTypeDAOImpl dao;
     @Inject
     @Named("sessionManagement")
-    private SessionManagementBean sessionManagement = new SessionManagementBean();
+    private transient SessionManagementBean sessionManagement;
 
-    @Override
-    public Sorting initSorting() {
-        return new Sorting("code", true);
+    @PostConstruct
+    public void init() {
+        dao = sessionManagement.getDAO(RbContragentTypeDAOImpl.class, ApplicationDAONames.RB_CONTRAGENT_TYPE_DAO);
+        setLazyModel(new LazyDataModelForContragentType(dao));
     }
-
-    @Override
-    protected int getTotalCount() {
-        try {
-            return new Long(sessionManagement.getDAO(RbContragentTypeDAOImpl.class, ApplicationDAONames
-                    .RB_CONTRAGENT_TYPE_DAO).countDocument(filter, false)).intValue();
-        } catch (Exception e) {
-            logger.error("Exception on count documents", e);
-            return 0;
-        }
-    }
-
-    @Override
-    protected List<ContragentType> loadDocuments() {
-        try {
-            return sessionManagement.getDAO(RbContragentTypeDAOImpl.class, ApplicationDAONames.RB_CONTRAGENT_TYPE_DAO).findDocuments(filter, false, -1, -1, getSorting().getColumnId(), getSorting().isAsc());
-
-        } catch (Exception e){
-            logger.error("Exception on get documents", e);
-            return Collections.emptyList();
-        }
-    }
-
-    public String getFilter() {
-        return filter;
-    }
-
-    public void setFilter(String filter) {
-        this.filter = filter;
-    }
-
 
 }
-
---
+++ code/war/src/main/java/ru/efive/dms/uifaces/lazyDataModel/referencebook/LazyDataModelForContragentType.java
@@ -0,0 +1,36 @@
+package ru.efive.dms.uifaces.lazyDataModel.referencebook;
+
+import org.primefaces.model.SortOrder;
+import ru.efive.dms.uifaces.lazyDataModel.AbstractFilterableLazyDataModel;
+import ru.efive.sql.dao.RbContragentTypeDAOImpl;
+import ru.entity.model.crm.ContragentType;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Author: Upatov Egor <br>
+ * Date: 20.04.2015, 16:00 <br>
+ * Company: Korus Consulting IT <br>
+ * Description: <br>
+ */
+public class LazyDataModelForContragentType extends AbstractFilterableLazyDataModel<ContragentType> {
+
+    private  RbContragentTypeDAOImpl dao;
+
+    public LazyDataModelForContragentType(final RbContragentTypeDAOImpl daoHibernate) {
+        dao = daoHibernate;
+    }
+
+    @Override
+    public ContragentType getRowData(String rowKey) {
+        return dao.get(Integer.valueOf(rowKey));
+    }
+
+    @Override
+    public List<ContragentType> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object>
+            filters) {
+        setRowCount((int) dao.countItems(getFilter(), false));
+        return dao.findItems(getFilter(), false, first, pageSize, sortField, sortOrder == SortOrder.ASCENDING);
+    }
+}
