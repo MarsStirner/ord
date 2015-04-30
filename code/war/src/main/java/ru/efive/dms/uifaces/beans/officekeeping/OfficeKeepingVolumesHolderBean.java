76120112da79de8948a2fcca1aff4cc6fbf18dfd
Upatov Egor <EUpatov@korusconsulting.ru>
2015-04-30 15:48:02 +0300

    glassfish 4.1 migration fixes

--- code/war/src/main/java/ru/efive/dms/uifaces/beans/officekeeping/OfficeKeepingVolumesHolderBean.java
+++ code/war/src/main/java/ru/efive/dms/uifaces/beans/officekeeping/OfficeKeepingVolumesHolderBean.java
@@ -1,5 +1,6 @@
 package ru.efive.dms.uifaces.beans.officekeeping;
 
+import org.apache.commons.lang.StringUtils;
 import ru.efive.dms.dao.OfficeKeepingVolumeDAOImpl;
 import ru.efive.dms.uifaces.beans.SessionManagementBean;
 import ru.efive.dms.uifaces.beans.abstractBean.AbstractDocumentLazyDataModelBean;
@@ -11,6 +12,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
 
 import static ru.efive.dms.util.ApplicationDAONames.OFFICE_KEEPING_VOLUME_DAO;
 
@@ -27,4 +30,17 @@ public class OfficeKeepingVolumesHolderBean extends AbstractDocumentLazyDataMode
         final OfficeKeepingVolumeDAOImpl dao = sessionManagement.getDAO(OfficeKeepingVolumeDAOImpl.class, OFFICE_KEEPING_VOLUME_DAO);
         setLazyModel(new LazyDataModelForOfficeKeepingVolume(dao));
     }
+
+    public List<OfficeKeepingVolume> getAllVolumesByParentFileId(String parentId) {
+        List<OfficeKeepingVolume> result = new ArrayList<OfficeKeepingVolume>();
+        try {
+            if (StringUtils.isNotEmpty(parentId)) {
+                result = sessionManagement.getDAO(OfficeKeepingVolumeDAOImpl.class, OFFICE_KEEPING_VOLUME_DAO).findAllVolumesByParentId(parentId);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 }
